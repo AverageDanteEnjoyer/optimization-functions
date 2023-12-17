df2d18255ec8c384d8e4666a5e5636b1c069d0f7
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2023-12-17 20:13:07 +0100

    finish ParticleSwarmIterator

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         String rosenbrockFnExpr = "(1-x1)^2+100*(x2-x1^2)^2";
-        ParticleSwarmIterator iter = new ParticleSwarmIterator(new double[][]{{-10, 10}, {-5, 5}}, rosenbrockFnExpr, 2, 2);
+        ParticleSwarmIterator iterator = new ParticleSwarmIterator(new double[][]{{-10, 10}, {-10, 10}}, rosenbrockFnExpr, 1.1, 1.3, 0.2);
+        double[] nextBest = null;
+        for(int i=0;i<100;i++){
+            double[] tmp = iterator.next();
+            if(tmp == nextBest)continue;
+            nextBest=tmp;
+            System.out.println(nextBest[0]);
+            System.out.println(nextBest[1]);
+            System.out.println(nextBest[2]);
+            System.out.print("\n");
+        }
     }
 }
--- src/ParticleSwarmIterator.java
+++ src/ParticleSwarmIterator.java
@@ -1,24 +1,29 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class ParticleSwarmIterator extends OptimizationIterator {
     public double c1;
     public double c2;
-    public int nParticles = 10;
+    public int nParticles = 800;
     public double[][] particlesVector;
     public double[][] velocityVector;
     public double[] gBest = null;
     public double[][] pBest;
-    public ParticleSwarmIterator(double[][] domain, String evalExpr, float c1, float c2){
+
+    public int epoch = 0;
+    public ParticleSwarmIterator(double[][] domain, String evalExpr, double c1, double c2, double initialVelocityMultiplier){
         super(domain, evalExpr);
         this.c1 = c1;
         this.c2 = c2;
-        this.velocityVector = generateInitialVector(0.2);
+        this.velocityVector = generateInitialVector(initialVelocityMultiplier);
         this.particlesVector = generateInitialVector(1);
         pBest = new double[nParticles][domain.length+1];
-        updateBestValues(true);
-        displayVector(this.particlesVector);
-        displayVector(this.pBest);
+    }
+
+    public double[] next(){
+        updateBestValues();
+        updateParticles();
+        epoch++;
+        return gBest;
     }
 
     public double[][] generateInitialVector(double domainMultiplier){
@@ -41,10 +46,10 @@ public class ParticleSwarmIterator extends OptimizationIterator {
         return this.targetFunction.evaluate(particle[0], particle[1]);
     }
 
-    public void updateBestValues(boolean isInitial){
+    public void updateBestValues(){
         for (int i = 0; i < nParticles; i++) {
             double functionValue = getFunctionValue(particlesVector[i]);
-            if(!isInitial && !(functionValue < pBest[i][domain.length]))continue;
+            if(!(epoch == 0) && !(functionValue < pBest[i][domain.length]))continue;
 
             int j;
             for (j = 0; j < domain.length; j++) {
@@ -53,6 +58,20 @@ public class ParticleSwarmIterator extends OptimizationIterator {
             pBest[i][j] = functionValue;
             if(gBest == null || pBest[i][domain.length] < gBest[domain.length]){
                 gBest = pBest[i];
+            }
+        }
+    }
+
+    public void updateParticles(){
+        Random random = new Random();
+        for (int i = 0; i < nParticles; i++) {
+            for (int j = 0; j < domain.length; j++) {
+                velocityVector[i][j] += (c1* random.nextDouble()*(pBest[i][j] - particlesVector[i][j])
+                        + c2*random.nextDouble()*(gBest[j]-particlesVector[i][j]));
+                double updatedParticle = particlesVector[i][j] + velocityVector[i][j];
+                if(updatedParticle > domain[j][0] && updatedParticle < domain[j][1]){
+                    particlesVector[i][j] = updatedParticle;
+                }
             }
         }
     }
