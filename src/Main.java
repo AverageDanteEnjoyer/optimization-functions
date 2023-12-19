a9e631561cd5c69e614ca73320858f92058eab98
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2023-12-19 14:31:43 +0100

    add customizable inputs

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String rosenbrockFnExpr = "(1-x1)^2+100*(x2-x1^2)^2";
-//        OptimizationIterator iterator = new ParticleSwarmIterator(new double[][]{{-1000, 1000}, {-1000, 1000}}, rosenbrockFnExpr, 1.3, 1.1, 0.2);
-        OptimizationIterator iterator = new SimulatedAnnealingIterator(new double[][]{{-100, 100}, {-100, 100}}, rosenbrockFnExpr, 0.95, 0.95, new double[]{15, -50});
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proszę podać funkcję (zmienne proszę nazwać x1, x2, ..., xn)");
+        String fnExpr = scanner.nextLine();
+        System.out.println("Proszę podać algorytm [SA/PSO]");
+        String iteratorType = scanner.nextLine();
+
+        OptimizationIterator iterator;
+        switch (iteratorType){
+            case "SA":
+                iterator = new SimulatedAnnealingIterator(new double[][]{{-10, 10}, {-10, 10}}, fnExpr, 0.9, 0.95, new double[]{5, -5});
+                break;
+            case "PSO":
+                iterator = new ParticleSwarmIterator(new double[][]{{-10, 10}, {-10, 10}}, fnExpr, 1.3, 1.1, 0.2);
+                break;
+            default:
+                System.out.println("Nie istnieje typ: " + iteratorType);
+                return;
+        }
+
         double[] nextBest = null;
         for(int i=0;i<1000;i++){
             double[] tmp = iterator.next();
-            if(tmp == nextBest)continue;
+            if(nextBest != null && tmp[tmp.length-1] == nextBest[tmp.length-1])continue;
             nextBest=tmp;
             System.out.println(i);
             System.out.println(nextBest[0]);
@@ -14,5 +33,10 @@ public class Main {
             System.out.println(nextBest[2]);
             System.out.print("\n");
         }
+        scanner.close();
+    }
+
+    private void iterateThroughEpochs(OptimizationIterator iterator, int epochs){
+
     }
 }
